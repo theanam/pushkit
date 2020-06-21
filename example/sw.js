21e7d12a9257d0bae7449610ed9f5588cb28a940
Anam Ahmed <anam.ahmed.a@gmail.com>
2020-06-22 04:48:47 +0600

    changed worker binding to handle click to open

--- example/sw.js
+++ example/sw.js
@@ -1,7 +1,8 @@
-importScripts("https://unpkg.com/pushkit@2.0.1/worker/binding.js"); 
+importScripts("https://unpkg.com/pushkit@2.0.5/worker/binding.js"); 
 var pushOptions = {
     title : "Pushkit Sample",
     icon  : "/pushkit/pushkit.png",
-    badge : "/pushkit/pushkit.png"
+    badge : "/pushkit/pushkit.png",
+    url   : "https://theanam.github.io/pushkit"
 }
 attachPushKit(self, pushOptions);
