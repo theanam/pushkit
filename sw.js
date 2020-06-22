0dda38d8bee9341d164768a9b402b200ceade5da
Anam Ahmed <anam.ahmed.a@gmail.com>
2020-06-22 22:17:59 +0600

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("binding.js"); 
+importScripts("https://unpkg.com/pushkit@3.0.1/worker/binding.js"); 
 var pushConfig = {
     icon  : "https://theanam.github.io/pushkit/pushkit.png",
     badge : "https://theanam.github.io/pushkit/pushkit.png"
