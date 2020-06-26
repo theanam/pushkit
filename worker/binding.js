10e0ca512af9d74d9935e3eb16425fa6c302c703
Anam Ahmed <anam.ahmed.a@gmail.com>
2020-06-27 05:16:27 +0600

    removed permission from SW. added to client

--- worker/binding.js
+++ worker/binding.js
@@ -64,27 +64,6 @@ function _reject(err,verbose){
     verbose && console.log("Permission failed", err);
 }
 function attachPushKit(scope, config, defaultTitle, defaultURL,customClickHandler, verbose){
-    scope.addEventListener("activate",()=>{
-        if(Notification && "requestPermission" in Notification){
-            // Suggested by: @fa7ad(https://github.com/fa7ad) for some older browser
-            verbose && console.log("Requesting Notification permission");
-            try{
-                verbose && console.log("Trying to use the promise based API");
-                Notification.requestPermission().then(function(_p){
-                    if(_p === "granted") return attach(scope, config, defaultTitle, defaultURL, customClickHandler, verbose);
-                    verbose && console.log("Permission failed");
-                }).catch(function(e){_reject(e, verbose)});
-            }
-            catch(e){
-                verbose && console.log("Trying to use the callback based API");
-                Notification.requestPermission(function(_p){
-                    if(_p === "granted") return attach(scope, config, defaultTitle, defaultURL, customClickHandler, verbose);
-                    verbose && console.log("Permission failed");
-                });
-            }
-        }
-        else{
-            return attach(scope, config, defaultTitle, defaultURL, customClickHandler, verbose);
-        }
-    });
+    // kept scope for activate or other stuff
+    return attach(scope, config, defaultTitle, defaultURL, customClickHandler, verbose);
 }
