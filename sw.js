b001a71a7afa06b8ddafb63d93ee960be2acb77f
Anam Ahmed <anam.ahmed.a@gmail.com>
2020-06-27 05:20:19 +0600

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-importScripts("https://unpkg.com/pushkit@3.2.0/worker/binding.js"); 
+importScripts("https://unpkg.com/pushkit@3.2.1/worker/binding.js"); 
 // importScripts("binding.js"); 
 var pushConfig = {
     icon  : "https://theanam.github.io/pushkit/pushkit.png",
     badge : "https://theanam.github.io/pushkit/pushkit.png"
 }
-attachPushKit(self, pushConfig,"Pushkit","http://localhost:1234",null,true);
+attachPushKit(self, pushConfig, "Pushkit", "http://localhost:1234", null, true);
